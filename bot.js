a9751f6797151809ccc4937601ba17ff2c22acaa
thangaraj26 <36671453+thangaraj26@users.noreply.github.com>
2020-07-09 19:33:27 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,46 @@
+const Telegraf = require('telegraf');
+const fs = require('fs');
+const bot = new Telegraf('881874108:AAFeV8zsZmxXXXkAuEmk7PWn8zlMyAr-IOc');
+var greet = ["Hi","Hello","hi","hello"];
+var data = fs.readFileSync("./data.json");
+var dictionary = JSON.parse(data);
+
+startMessage = '\nUsage:/find <word> - To find the meaning\nCreator: Thangaraj';
+
+bot.start((ctx)=>{
+    ctx.reply("welcome "+ctx.from.first_name+startMessage);
+    
+})
+
+bot.help((ctx)=>{
+    ctx.reply("Its the helpline");
+})
+
+bot.settings((ctx)=>{
+    ctx.reply("Settings Enter one for Main menu");
+})
+
+
+bot.command(["Chat","chat","CHAT"],(ctx)=>{
+    ctx.reply("Let's Chat I'm the BOT");
+})
+bot.command(["find","Find","FIND"],(ctx)=>{
+    let input = ctx.message.text;
+    let inputArray = input.split(" ");
+    let message = "";
+    if(inputArray.length == 1){
+        message = "Type the word....";
+    }
+    else{
+        inputArray.shift();
+        key = inputArray.join(" ");
+        message = dictionary[key];
+        message = String(message);
+    }
+    ctx.reply(message);
+})
+
+bot.hears(greet,(ctx)=>{
+    ctx.reply("Hi "+ ctx.from.first_name);   
+})
+bot.launch();
